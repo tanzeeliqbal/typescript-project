bb2ee3192f49c779ffb68a8c61ea226374a0e40b
tanzeeliqbal <162276170+tanzeeliqbal@users.noreply.github.com>
2024-03-05 11:03:19 +0500

    Add files via upload

---
+++ ass05.js
@@ -0,0 +1,19 @@
+//camelCase
+//snake_case
+//PascalCase
+var userName = "Tanzeel";
+var user_name = "Tanzeel";
+var UserName = "Tanzeel";
+// = is assignment operator
+var isUserExist = true;
+var is_user_Exist = false;
+var IsUserExist = 1;
+//console.log (userName , user_name , UserName)
+//console.log (isUserExist , is_user_Exist , IsUserExist)
+var anytype;
+//console.log (anytype)
+// arrays [] for arrays we use square brackets
+var students = ["Noman", "Tanzeel", "Ahmed", 1, 10, true];
+var firststudentname = students[5];
+console.log(students[5]);
+//console.log (firststudentname)
---
+++ ass05.ts
@@ -0,0 +1,22 @@
+//camelCase
+//snake_case
+//PascalCase
+let userName : string = "Tanzeel"
+let user_name : string = "Tanzeel"
+let UserName : string = "Tanzeel"
+// = is assignment operator
+let isUserExist : boolean = true;
+let is_user_Exist : boolean = false;
+let IsUserExist : number = 1 ;
+//console.log (userName , user_name , UserName)
+//console.log (isUserExist , is_user_Exist , IsUserExist)
+let anytype:any;
+//console.log (anytype)
+// arrays [] for arrays we use square brackets
+
+let students = ["Noman","Tanzeel","Ahmed",1 , 10 ,true]
+let firststudentname = students[5]
+console.log (students[5])
+//console.log (firststudentname)
+
+

9a3cd6db6f0bdc23fdc55ba89b9b3d95165d45a4
tanzeeliqbal <162276170+tanzeeliqbal@users.noreply.github.com>
2024-03-09 17:30:08 +0500

    Add files via upload

---
+++ ass1.ts
@@ -0,0 +1,36 @@
+//let fullname = "Tanzeeel"
+//console.log (fullname);
+//let fullname : string = "Tanzeeel";
+//console.log (fullname);
+//let a = 15
+//let b = 20
+//console.log (a ,"+",b,"=" ,a + b)
+//let personage : number = 30
+//let finalage : number = personage + 5 ;
+//console.log ("Person age is " , finalage)
+//let a = 10
+//let b: boolean = a<20
+//console.log(b)
+//let d = null
+//console.log (d)
+//let g = undefined
+//console.log(g)
+//camalCase
+//let userName : string = "Tanzeel"
+//console.log(userName)
+//snake_case
+//let user_name : string = "Tanzeel"
+//console.log(user_name)
+//PascalCase
+//let UserName : string = "Tanzeel"
+//console.log(UserName)
+let students: string [] = ["Noman","Tanzeel","Ahmed","Ali","Kashif" ]
+let firststudentname = students[4]
+console.log (students[2])
+console.log (firststudentname)
+console.log(students[1],students[2])
+let fruits: string[] = ["apple" , "Banana" , "Orange"]
+console.log (fruits[2])
+fruits[1] = "Mange"
+console.log (fruits[1])
+
